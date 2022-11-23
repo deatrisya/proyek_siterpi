e9b157da34f2a08d4cdff734e729deeee192fc0c
deatrisya <mirelldee47@gmail.com>
2022-11-23 09:06:45 +0700

    edit navbar and sidebar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -24,7 +24,7 @@
         <a href="javascript:;" class="nav-link text-body p-0" type="button" id="dropdownMenuButton"
             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="{{asset('admin/img/profile-img.jpg')}}" alt="Profile" class="rounded-circle">
+            <img src="{{asset('storage/'.Auth()->user()->foto)}}" alt="Profile" class="rounded-circle">
             <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->name }}</span>
           </a><!-- End Profile Iamge Icon -->
 
